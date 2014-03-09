e236281a0b4cee7308e8651785932e25428f5c6b
appmite <admin@appmite.com>
2014-03-08 22:40:01 -0500

    collection: Begin collection class and tests

---
+++ spec/collection.test.js
@@ -0,0 +1,11 @@
+describe('Collection', function () {
+
+    var Collection;
+
+    beforeEach(function() {
+        Collection = require('../src/collection');
+    });
+
+
+
+});
---
+++ src/collection.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Anthony on 3/7/14.
+ */
